43cb0507ecd45ac8cff435fad6220b66882ae371
Dmitry Aladin <anon.digriz@gmail.com>
2023-04-12 02:27:44 +0300

    Beginning of the project description

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import Translate from "@docusaurus/Translate";
+
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Learn about how to use it 🚀
+            to="/docs/intro"
+          >
+            <Translate id="homepage.button.label" description="The homepage main button">
+              Learn about how to use it 🚀
+            </Translate>
           </Link>
         </div>
       </div>
@@ -27,11 +32,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="mogan-mini implements a local Editor of the Multidimensional Open Gnoseological Active Network (MOGAN) on macOS, Linux, and Windows.">
+      description="mogan-mini implements a local Editor of the Multidimensional Open Gnoseological Active Network (MOGAN) on macOS, Linux, and Windows."
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
